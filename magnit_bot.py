d2c408b8bc7bc12d8c588d7fffca724d57bd0691
Irina4567 <irina.pavliuchenkova@yandex.ru>
2025-01-28 11:09:34 +0700

    TEST

--- magnit_bot.py
+++ magnit_bot.py
@@ -12,7 +12,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    start_buttons = ['Moscow', 'Ekaterinburg']
+    start_buttons = ['Moscow']
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
     await message.answer('Please select a City', reply_markup=keyboard)
@@ -25,18 +25,10 @@ async def moscow_city(message: types.Message):
     await send_data(city_code='2398', chat_id=chat_id)
 
 
-@dp.message_handler(Text(equals='Ekaterinburg'))
-async def ekb_city(message: types.Message):
-    await message.answer('Please waiting...')
-    chat_id = message.chat.id
-    await send_data(city_code='1869', chat_id=chat_id)
-
-
 async def send_data(city_code='', chat_id=''):
-    file = await collect_data(city_code=city_code)
-    print(file)
-#     await bot.send_document(chat_id=chat_id, document=open(file, 'rb'))
-#     await os.remove(file)
+#     file = await collect_data(city_code=city_code)
+#     print(file)
+    bot.send_message(chat_id, 'TEST')
 
 
 if __name__ == '__main__':
