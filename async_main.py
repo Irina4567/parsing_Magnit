648e5250ef76b7803a58061340f122204b38b18d
Irina4567 <irina.pavliuchenkova@yandex.ru>
2025-01-28 11:04:07 +0700

    TEST

--- async_main.py
+++ async_main.py
@@ -16,74 +16,14 @@ async def collect_data(city_code='2398'):
         'User-Agent': ua.random
     }
 
-    cookies = {
-        'mg_geo_id': f'{city_code}'
-    }
 
     async with aiohttp.ClientSession() as session:
 
-        response = await session.get(url='https://magnit.ru/promo/',
-                                     headers=headers, cookies=cookies)
+        response = await session.get(url='https://www.wildberries.ru/',
+                                     headers=headers)
         soup = BeautifulSoup(await response.text(), 'lxml')
 
-        city = soup.find('a', class_='header__contacts-link_city').text.strip()
-        cards = soup.find_all('a', class_='card-sale_catalogue')
-
-
-        print(city)
-#         data = []
-#         for card in cards:
-#             card_title = card.find('div',
-#                                    class_='card-sale__title'
-#                                    ).text.strip()
-#
-#             try:
-#                 card_discount = card.find('div',
-#                                           class_='card-sale__discount'
-#                                           ).text.strip()
-#             except AttributeError:
-#                 continue
-#
-#             card_price_old_integer = card.find('div',
-#                                                class_='label__price_old').find(
-#                 'span', class_='label__price-integer').text.strip()
-#             card_price_old_decimal = card.find('div',
-#                                                class_='label__price_old').find(
-#                 'span', class_='label__price-decimal').text.strip()
-#             card_old_price = f'{card_price_old_integer}.{card_price_old_decimal}'
-#
-#             card_price_integer = card.find('div',
-#                                            class_='label__price_new').find(
-#                 'span', class_='label__price-integer').text.strip()
-#             card_price_decimal = card.find('div',
-#                                            class_='label__price_new').find(
-#                 'span', class_='label__price-decimal').text.strip()
-#             card_price = f'{card_price_integer}.{card_price_decimal}'
-#
-#             card_sale_date = card.find('div',
-#                                        class_='card-sale__date').text.strip().replace(
-#                 '\n', ' ')
-#
-#             data.append(
-#                 [card_title, card_discount, card_old_price, card_price,
-#                  card_sale_date]
-#             )
-
-#     async with aiofiles.open(f'{city}_{cur_time}.csv', 'w') as file:
-#         writer = AsyncWriter(file)
-#
-#         await writer.writerow(
-#             [
-#                 'Продукт',
-#                 'Старая цена',
-#                 'Новая цена',
-#                 'Процент скидки',
-#                 'Время акции',
-#             ]
-#         )
-#         await writer.writerows(
-#             data
-#         )
+        print(soup)
 
     return true
 
